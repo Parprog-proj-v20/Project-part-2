a8397436ee3a0529c7431e5a2c40b340c5fe9331
Kurochkina Anastasia <152472159+AAK30-23@users.noreply.github.com>
2025-11-16 16:46:05 +0300

    Добавлен тест на граничные случаи MatrixTest.cpp

--- tests/MatrixTest.cpp
+++ tests/MatrixTest.cpp
@@ -47,3 +47,30 @@ void MatrixTest::testDifferentSizes() {
         assert(areMatricesEqual(matrix.C, expected));
     }
 }
+
+
+void MatrixTest::testEdgeCases() {
+    std::cout << "Тестирование граничных случаев" << std::endl;
+    
+    // Тест 1: Матрица 1x1
+    Matrix matrix1(1);
+    matrix1.A = {{5}};
+    matrix1.B = {{3}};
+    matrix1.multiplyParallel(2, "static");
+    assert(matrix1.C[0][0] == 15);
+
+    // Тест 2: Нулевая матрица
+    Matrix matrix2(3);
+    matrix2.A = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+    matrix2.B = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    std::vector<std::vector<int>> expected = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+    matrix2.multiplyParallel(2, "static");
+    assert(areMatricesEqual(matrix2.C, expected));
+
+    // Тест 3: Единичная матрица
+    Matrix matrix3(3);
+    matrix3.A = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    matrix3.B = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+    matrix3.multiplyParallel(2, "static");
+    assert(areMatricesEqual(matrix3.C, matrix3.A));
+}
