33f0983fda45ce8e1b405a3e188d1e25147aba4f
saint-just <93088895+AnnVusatyuk@users.noreply.github.com>
2025-11-16 11:39:26 +0300

    Добавлено разбиение на файлы

---
+++ src/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include "Matrix.h"
+#ifdef _WIN32 
+#include <windows.h>
+#endif
+
+/**
+* @brief Создание матриц установленных размеров и их перемножение при разном количестве
+* потоком и разных типах планировок. Вызывает функции для получения времени линейного
+* перемножения и однопоточного перемножения с планировкой static, затем сравнивает время
+* выполнения и рассчитывает ускорение.
+*/
+void run() {
+    double time, speedup, linear_speedup;
+    std::vector<size_t> sizes = { 500, 600, 800, 1000, 1200 };
+    std::vector<int> thread_counts = { 1, 2, 4, 8 };
+    std::vector<std::string> types = { "static", "dynamic", "guided" };
+    std::cout << std::string(60, '-') << std::endl;
+
+    for (int size : sizes) {
+        std::cout << "\n\tПЕРЕМНОЖЕНИЕ МАТРИЦ: A[" << size << "x" << size << "] * B[" << size << "x" << size << "]\n" << std::endl;
+        std::cout << std::string(60, '-') << std::endl;
+
+        Matrix m(size);
+        m.initialize();
+
+        double linear_time = m.multiplyLinear();
+        double single_thread_time = m.multiplyParallel(1, "static");
+        std::cout << "> ЛИНЕЙНОЕ ПЕРЕМНОЖЕНИЕ, время выполнения: " << linear_time << " сек" << std::endl;
+        std::cout << "> 1 ПОТОК, время выполнения: " << single_thread_time << " сек" << std::endl;
+        std::cout << std::string(60, '-') << std::endl;
+
+        for (int threads : thread_counts) {
+            std::cout << "> КОЛИЧЕСТВО ПОТОКОВ: " << threads << std::endl;
+
+            for (const auto& schedule : types) {
+                time = m.multiplyParallel(threads, schedule);
+                speedup = single_thread_time / time;
+                linear_speedup = linear_time / time;
+
+                std::cout << "\n> ПЛАНИРОВКА: " << schedule << "\n> ВРЕМЯ: " << time
+                    << " сек\n\tУскорение по отнош. к линейному: " << linear_speedup << "\n\tУскорение по отнош.к 1 потоку: " << speedup << std::endl;
+            }
+            std::cout << std::string(60, '-') << std::endl;
+        }
+        std::cout << "\n" << std::string(60, '-') << std::endl;
+    }
+}
+
+int main() {
+    #ifdef _WIN32
+    SetConsoleOutputCP(65001);
+    SetConsoleCP(65001);
+    #endif
+
+    run();
+    return 0;
+}
