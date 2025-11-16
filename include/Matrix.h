17da0670f0dc5b07daa8fd9a0bcfc080c165c2b9
saint-just <93088895+AnnVusatyuk@users.noreply.github.com>
2025-11-17 00:03:07 +0300

    Добавлены геттеры и сеттеры для матриц

--- include/Matrix.h
+++ include/Matrix.h
@@ -21,6 +21,37 @@ public:
     Matrix(int n);
 
     /**
+    * @brief Сеттер первой матрицы A. Копирует матрицу newA во внутреннюю переменную класса.
+    *
+    * @param newA - матрица, которая будет скопирована в A 
+    */
+    void setMatrixA(const std::vector<std::vector<int>>& newA); 
+    /**
+    * @brief Сеттер второй матрицы B. Копирует матрицу newB во внутреннюю переменную класса.
+    *
+    * @param newB - матрица, которая будет скопирована в B
+    */
+    void setMatrixB(const std::vector<std::vector<int>>& newB);
+    /**
+    * @brief Геттер первой матрицы A.
+    *
+    * @return константная ссылка на матрицу A
+    */
+    const std::vector<std::vector<int>>& getMatrixA() const; 
+    /**
+    * @brief Геттер второй матрицы B.
+    *
+    * @return константная ссылка на матрицу B
+    */
+    const std::vector<std::vector<int>>& getMatrixB() const; 
+    /**
+    * @brief Геттер результирующей матрицы C.
+    *
+    * @return константная ссылка на матрицу C
+    */
+    const std::vector<std::vector<int>>& getMatrixC() const;
+
+    /**
     * @brief Заполнение матриц A и B случайными числами в диапазоне [-100, 100].
     */
     void initialize();
